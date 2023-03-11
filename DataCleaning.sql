0900fc8745283f829a01197cff7e452ee602f42a
Iriss <105106431+iriss-bit@users.noreply.github.com>
2023-03-11 13:37:07 +0200

    Update DataCleaning.sql

--- DataCleaning.sql
+++ DataCleaning.sql
@@ -1,28 +1,36 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
----------------------------------sale date-------------------------------------------------
-SELECT 
-       [SaleDate],
-	   SaleDateConverted
-FROM [SqlTutorial].[dbo].[Housing];
+---------------------------------SaleDate Cleaning Column-------------------------------------------------
+Step 1:
 
 ALTER TABLE [SqlTutorial].[dbo].[Housing]
 ADD SaleDateConverted date;
 
+Step 2:
+
 UPDATE [SqlTutorial].[dbo].[Housing]
 SET SaleDateConverted = convert(date,SaleDate);
----------------------------------Proprety Address-------------------------------------------------
+
+Step 3:
+
+SELECT
+   SaleDateConverted
+FROM [SqlTutorial].[dbo].[Housing]
+---------------------------------Proprety Address Cleaning-------------------------------------------------
+Step1
+
 SELECT 
      a.ParcelID,
-	 b.ParcelID,
-	 a.PropertyAddress,
-	 b.PropertyAddress,
-	 isnull(a.PropertyAddress,b.PropertyAddress) 
+     b.ParcelID,
+     a.PropertyAddress,
+     b.PropertyAddress,
+     isnull(a.PropertyAddress,b.PropertyAddress) 
 FROM [SqlTutorial].[dbo].[Housing] a
 JOIN [SqlTutorial].[dbo].[Housing] b
 ON a.ParcelID= b.ParcelID
 AND a.UniqueID <> b.UniqueID
 WHERE a.PropertyAddress is null;
 
+Step 2
 
 UPDATE a
 SET PropertyAddress=isnull(a.PropertyAddress,b.PropertyAddress)
@@ -32,23 +40,30 @@ ON a.ParcelID= b.ParcelID
 AND a.UniqueID <> b.UniqueID
 WHERE a.PropertyAddress is null;
 ---------------------------------substring/charindex-------------------------------------------------
-SELECT 
-     *
-FROM [SqlTutorial].[dbo].[Housing];
+Step1:
 
 SELECT 
-	 SUBSTRING(PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1)as address,
-	 SUBSTRING(PropertyAddress,(CHARINDEX(',',PropertyAddress)+1),LEN(PropertyAddress)) as city
-FROM [SqlTutorial].[dbo].[Housing];
+      SUBSTRING(PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1)as address,
+      SUBSTRING(PropertyAddress,(CHARINDEX(',',PropertyAddress)+1),LEN(PropertyAddress)) as city
+ FROM [SqlTutorial].[dbo].[Housing];
 
-
+Step 2:
 ALTER TABLE [SqlTutorial].[dbo].[Housing]
 ADD PropertyAddressSplit varchar(255),
-	ProperSplitCity varchar(255); 
+    ProperSplitCity varchar(255); 
+    
+Step3:
 
 UPDATE [SqlTutorial].[dbo].[Housing]
 --SET PropertyAddressSplit=SUBSTRING(PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1)
-SET ProperSplitCity=SUBSTRING(PropertyAddress,(CHARINDEX(',',PropertyAddress)+1),LEN(PropertyAddress))
+SET ProperSplitCity=SUBSTRING(PropertyAddress,(CHARINDEX(',',PropertyAddress)+1),LEN(PropertyAddress));
+
+
+Step 4:
+
+SELECT 
+     *
+FROM [SqlTutorial].[dbo].[Housing];
 
 ---------------------------------substring/charindex Owner Address-------------------------------------------------
 SELECT 
